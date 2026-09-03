8abd6d78eace8acf08f8d6c367b35fe7a0b0b35d
esraras <eesraras29@gmail.com>
2026-09-03 16:35:14 +0300

    vercel.json

--- server/server.js
+++ server/server.js
@@ -48,7 +48,6 @@ app.use((req, res) => {
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(5000, () => console.log('Server running locally'));
+}
